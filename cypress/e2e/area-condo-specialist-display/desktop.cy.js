bb63432561153fce0a8dba06152fdd4986745bac
kesontan <93306607+kesontan@users.noreply.github.com>
2022-07-18 17:09:18 +0800

    Update desktop.cy.js

--- cypress/e2e/area-condo-specialist-display/desktop.cy.js
+++ cypress/e2e/area-condo-specialist-display/desktop.cy.js
@@ -14,58 +14,28 @@ const phoneNumberSelector = "div[class^AgentSpecialistCardstyle__PhoneNumber]";
 
 const AREA_AGENTS_DATA = [
   {
-    name: "Gwen Ng",
-    contact: "+60169173331",
-    agency: "Apexe Realty",
-    channelCounts: { sale: 4, rent: 7 },
-  },
-  {
     name: "Daniel Yong",
     contact: "+60163375377",
     agency: "Dutama Properties",
-    channelCounts: { sale: 123, rent: 46 },
-  },
-  {
-    name: "Merchel Liew",
-    contact: "+60192898346",
-    agency: "Propnex Realty Sdn. Bhd.",
-    channelCounts: { sale: 17, rent: 42 },
-  },
-  {
-    name: "Sylvia Chin",
-    contact: "+60163231471",
-    agency: "Propnex Realty Sdn. Bhd.",
-    channelCounts: { sale: 44, rent: 48 },
-  },
-  {
-    name: "Patriciar Tham",
-    contact: "+60196898343",
-    agency: "Propnex Realty Sdn. Bhd.",
-    channelCounts: { sale: 17, rent: 33 },
-  },
-  {
-    name: "Francis Ngooi",
-    contact: "+601165185155",
-    agency: "Reapfield Properties (HQ) Sdn. Bhd.",
-    channelCounts: { sale: 43, rent: 24 },
-  },
-  {
-    name: "Mabel Mak",
-    contact: "+60129717115",
-    agency: "Propnex Realty Sdn. Bhd. - KL",
-    channelCounts: { sale: 8, rent: 6 },
-  },
-  {
-    name: "Dylan Ng",
-    contact: "+60166661847",
-    agency: "Greenfield Properties - Kota Damansara",
-    channelCounts: { sale: 39, rent: 48 },
+    channelCounts: { sale: 177, rent: 25 },
   },
   {
     name: "Elsis Liew",
     contact: "+60189880204",
     agency: "Propnex Realty Sdn. Bhd.",
-    channelCounts: { sale: 11, rent: 8 },
+    channelCounts: { sale: 13, rent: 8 },
+  },
+  {
+    name: "Sylvia Chin",
+    contact: "+60163231471",
+    agency: "Propnex Realty Sdn. Bhd.",
+    channelCounts: { sale: 37, rent: 43 },
+  },
+  {
+    name: "CT Weng",
+    contact: "+60126343199",
+    agency: "The Roof Realty - Puchong",
+    channelCounts: { sale: 5, rent: 1 },
   },
   {
     name: "Nicole How",
@@ -74,10 +44,16 @@ const AREA_AGENTS_DATA = [
     channelCounts: { sale: 14, rent: 0 },
   },
   {
-    name: "CT Weng",
-    contact: "+60126343199",
-    agency: "The Roof Realty - Puchong",
-    channelCounts: { sale: 3, rent: 0 },
+    name: "Patriciar Tham",
+    contact: "+60196898343",
+    agency: "Propnex Realty Sdn. Bhd.",
+    channelCounts: { sale: 18, rent: 34 },
+  },
+  {
+    name: "Francis Ngooi",
+    contact: "+601165185155",
+    agency: "Reapfield Properties (HQ) Sdn. Bhd.",
+    channelCounts: { sale: 29, rent: 21 },
   },
 ];
 
