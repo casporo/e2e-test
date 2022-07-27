a5b7d15d21411701be96a7476bc7cfe96e2a8735
Casporo <casper.g.kaun@gmail.com>
2022-07-27 16:29:46 +0800

    - Added cypress wait, possible implementation for cards scroll & also total count of element and compare it with current data

--- cypress/e2e/area-condo-specialist-display/mobile.cy.js
+++ cypress/e2e/area-condo-specialist-display/mobile.cy.js
@@ -5,6 +5,13 @@ const skeletonSelector =
 const containerSelector =
   "div[class^=AgentSpecialistAsyncComponentsstyle__AgentSpecialistCarouselPlaceholder]";
 
+const ApartmentfifthCard = "div[class^=ListingHeadingstyle__HeadingTitle-bPnUeN bwCNIt]";
+
+const CondoFifthCard = "div[class^=ListingHeadingstyle__HeadingTitle-bPnUeN cYvHoO]";
+const CondoNinthCard = "div[class^=ListingHeadingstyle__HeadingTitle-bPnUeN cYvHoO]";
+
+
+
 // TODO: find title
 const titleSelector = "div[class^=Filtersstyle__Row-DpPge OPMhk]";
 
@@ -59,7 +66,8 @@ describe(
       cy.get(containerSelector).should("not.inViewport");
 
       // TODO: scroll after 5th cards
-      
+     cy.get('ApartmentfifthCard').scrollTo('bottom', { ensureScrollable: false })
+
 
       // TODO: if api still loading, skeleton exist else container
       
@@ -76,16 +84,32 @@ describe(
       cy.get(containerSelector).should("not.inViewport");
 
       // TODO: scroll after 5th cards
+      cy.get('CondoFifthCard').scrollTo('bottom', { ensureScrollable: false })
 
       // TODO: if api still loading, skeleton exist else container
-
+      cy.wait(skeletonSelector)
       cy.get(titleSelector).should("contain", "Condo specialists for");
 
       // TODO: all title should be same total count as CONDO_AGENTS_DATA
+      //To try and get all the agency name and compare with what is presented in mobile
+                 /*
+                 cy.task("generateJSONfromHeader", { CONDO_AGENTS_DATA })
+                       .then((users) => {
+                         dataList = [...users].map(user => user.agency)
+                       }))
+
+                   it('logs the loaded dataList', () => {
+                          cy.log(dataList)
+                          cy.log(dataList.length)
+                   })
+      cy.get(agencyNameSelector).find(datalist).should('have.length', datalist.length) */
+
 
       // TODO: scroll after 9th cards
+     cy.get('CondoNinthCard').scrollTo('bottom', { ensureScrollable: false })
 
       // TODO: if api still loading, skeleton exist else container
+      cy.wait(skeletonSelector)
 
       cy.get(containerSelector)
         .find(titleSelector)
